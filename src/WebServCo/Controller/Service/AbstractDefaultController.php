27d7185f211040555dd2192bdedf4d985e1e392d
Radu Maier <ariademur@webserv.co>
2024-01-22 08:21:05 +0100

    Handle non-templated-text (HTML) output.

--- src/WebServCo/Controller/Service/AbstractDefaultController.php
+++ src/WebServCo/Controller/Service/AbstractDefaultController.php
@@ -14,6 +14,7 @@ use WebServCo\DependencyContainer\Contract\LocalDependencyContainerInterface;
 use WebServCo\DependencyContainer\Helper\ApplicationDependencyServiceAccessTrait;
 use WebServCo\Http\Contract\Message\Response\StatusCodeServiceInterface;
 use WebServCo\View\CommonView;
+use WebServCo\View\Contract\HTMLRendererInterface;
 use WebServCo\View\Contract\TemplateServiceInterface;
 use WebServCo\View\Contract\ViewContainerInterface;
 use WebServCo\View\Contract\ViewServicesContainerInterface;
@@ -72,6 +73,11 @@ abstract class AbstractDefaultController implements ControllerInterface
         return $this->commonView;
     }
 
+    /**
+     * Create main view container along with the template to use.
+     *
+     * Should be called from implementing code.
+     */
     protected function createMainViewContainerWithTemplate(
         ServerRequestInterface $request,
         string $templateName,
@@ -147,6 +153,21 @@ abstract class AbstractDefaultController implements ControllerInterface
         ServerRequestInterface $request,
         ViewContainerInterface $viewContainer,
     ): ViewContainerInterface {
+        /**
+         * Handle non-templated-text (HTML) output.
+         *
+         * Examples: JSON, PDF, multimedia, etc.
+         *
+         * In this case:
+         * - do not create a main View container,
+         * - instead simply return the existing View container.
+         *
+         * Otherwise: main view data would be rendered as a string and placed in the `data` of the main view.
+         */
+        if ($this->viewServicesContainer->getViewRenderer()->getContentType() !== HTMLRendererInterface::CONTENT_TYPE) {
+            return $viewContainer;
+        }
+
         // Set template service (template group information) to use.
         $templateService = $this->createTemplateService(
             $this->getConfigurationGetter()->getString(
@@ -155,7 +176,10 @@ abstract class AbstractDefaultController implements ControllerInterface
         );
         $viewContainer->setTemplateService($templateService);
 
-        // Create main View (general page layout containing also the rendered page template).
+        /**
+         * Create main View (general page layout containing also the rendered page template).
+         * `createMainViewContainer` is located in implementing code.
+         */
         $mainViewContainer = $this->createMainViewContainer($request, $viewContainer);
         // Set template service (template group information) to use.
         $mainViewContainer->setTemplateService($templateService);
